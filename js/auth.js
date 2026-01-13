0f5d1839c5ca9d3d2c2e95499587075495eec32f
HDan9304 <101402467+HDan9304@users.noreply.github.com>
2026-01-13 20:05:38 +0800

    Add signup and login functions for authentication

---
+++ js/auth.js
@@ -0,0 +1,25 @@
+function signup() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(error => {
+      document.getElementById("message").innerText = error.message;
+    });
+}
+
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(error => {
+      document.getElementById("message").innerText = error.message;
+    });
+}
